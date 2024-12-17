46f68f42c6afc9dde8d16d0237155ac6e5b73c4b
Thomas Ottosson <tomasottosson1@gmail.com>
2024-12-17 14:03:24 +0000

    model

--- blog/admin.py
+++ blog/admin.py
@@ -3,3 +3,12 @@ from .models import Post
 
 # Register your models here.
 admin.site.register(Post)
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
