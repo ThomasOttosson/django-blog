8a1efa6914b7de8e55ad06f11a78d2999a6a8d41
Thomas Ottosson <tomasottosson1@gmail.com>
2025-01-04 17:12:15 +0000

    test

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-thomasottoss-djangoblog-ksdz8hw7ul0.ws.codeinstitute-ide.net',
 ".herokuapp.com"]
